188ca313b457f38dc0204522e2322c76140f9ac8
Eduardo-HortaS <68471383+Eduardo-HortaS@users.noreply.github.com>
2023-04-19 22:40:27 -0300

    Django prototype, frontend training, finished.

--- prototypeManga/prototypeManga/settings.py
+++ prototypeManga/prototypeManga/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'prototypeManga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [
+            'templates',
+            'prototypeApp'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
