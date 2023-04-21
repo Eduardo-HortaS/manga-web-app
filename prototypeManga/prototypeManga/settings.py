7148cacefd3989d53c126764e844e5d9b53f0fa3
Eduardo-HortaS <68471383+Eduardo-HortaS@users.noreply.github.com>
2023-04-21 17:15:03 -0300

    Django prototype, frontend training, some issues with table styling and scripting, review pending.

--- prototypeManga/prototypeManga/settings.py
+++ prototypeManga/prototypeManga/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +58,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             'templates',
-            'prototypeApp'
+            'templates/partials/'
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,12 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/css/' Antes, voltar se der ruim.
-STATIC_URL = '/prototypeApp/static/'
-STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'prototypeApp/static'),
-] 
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
